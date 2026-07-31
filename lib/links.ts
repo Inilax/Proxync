50b5b576069fcb5cef5103aabb0557f04fccefc7
Krish <e63@bmsit.in>
2026-08-01 03:19:32 +0530

    feat: add app icon, class utility, and download links

---
+++ lib/links.ts
@@ -0,0 +1,4 @@
+export const GITHUB_URL = "https://github.com/Inilax/Proxync/";
+
+export const DOWNLOAD_URL =
+  "https://github.com/Inilax/Proxync/releases/download/v0.1.7/Proxync_0.1.7_x64-setup.exe";
